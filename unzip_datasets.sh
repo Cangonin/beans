b746bf4cac37a6e521998fabb3402fbcedaabf76
cangonin <celine.angonin71@gmail.com>
2025-06-03 17:39:26 +0200

    add slurm script

---
+++ unzip_datasets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -p GPU # partition (queue)
+#SBATCH -N 1 # number of nodes
+#SBATCH -t 0-36:00 # time (D-HH:MM)
+#SBATCH -o slurm.%N.%j.out # STDOUT
+#SBATCH -e slurm.%N.%j.err # STDERR
+
+python scripts/unzip_datasets.sh
